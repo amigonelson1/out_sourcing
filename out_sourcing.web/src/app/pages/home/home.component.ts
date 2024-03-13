08cb1dc87b51a93915e3823f790b731cc1f6ed1d
Nelson Amaris <amigonelson1@hotmail.com>
2024-03-12 21:43:14 -0500

    otro commit

--- out_sourcing.web/src/app/pages/home/home.component.ts
+++ out_sourcing.web/src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponentComponent } from '../../menu-component/menu-component.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MenuComponentComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
