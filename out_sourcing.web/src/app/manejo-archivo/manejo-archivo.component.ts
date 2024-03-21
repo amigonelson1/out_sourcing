0f5d2984d218545c2f9bf8600756e2c601d4b560
unknown <amigonelson1@hotmail.com>
2024-03-20 22:05:55 -0500

    first commit

--- out_sourcing.web/src/app/manejo-archivo/manejo-archivo.component.ts
+++ out_sourcing.web/src/app/manejo-archivo/manejo-archivo.component.ts
@@ -1,10 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
+
 
 //primeNg
 import { ButtonModule } from 'primeng/button';
 import { FileUploadModule } from 'primeng/fileupload';
 import { MessageService } from 'primeng/api';
+import { compileNgModule } from '@angular/compiler';
 
 @Component({
   selector: 'app-manejo-archivo',
@@ -35,7 +38,18 @@ export class ManejoArchivoComponent {
     for (let file of event.files) {
       this.uploadedFiles.push(file);
     }
-
     this.messageService.add({ severity: 'info', summary: 'File Uploaded', detail: '' });
+    console.log('la data del excel',this.uploadedFiles)
+    const excelFile = XLSX.readFile(this.uploadedFiles[0])
+    var nameSheet= excelFile.SheetNames
+    let datos = XLSX.utils.sheet_to_json(excelFile.Sheets[nameSheet[0]])
+    console.log('la data del excel', datos)
   }
+
+  loadXlsxFile() {
+
+  }
+
+
+
 }
